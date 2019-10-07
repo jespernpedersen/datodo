1a35453605d53d75f8b0af13178e54fb01f6c4d2
Jesper N. Pedersen <jeppenp12@gmail.com>
2019-10-07 12:33:48 +0200

    New Feature: Editing

--- src/components/BaseInputText.vue
+++ src/components/BaseInputText.vue
@@ -34,10 +34,6 @@ export default {
 .input {
   width: 100%;
   padding: 8px 10px;
-  // border: 1px solid $vue-blue;
-}
-
-ul {
-  list-style: none;
+  border: 1px solid $vue-blue;
 }
 </style>
--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,19 +1,42 @@
 <template>
   <li>
-    {{ todo.text }}
+    <!-- View before editing -->
+    <div v-show="!isEditing">
+      <span>{{ todo.text }}</span>
+    </div>
+    <!-- View in editing -->
+    <div v-show="isEditing">
+      <input v-show="isEditing" v-model="todo.text">
+    </div>
+    <!-- Action buttons -->
     <button @click="$emit('remove', todo.id)">
       X
+    </button>
+    <button v-on:click="showForm" v-show="!isEditing">
+      Edit
+    </button>
+    <button v-on:click="hideForm" v-show="isEditing">
+      Complete
     </button>
   </li>
 </template>
 
 <script>
+
 export default {
-  props: {
-    todo: {
-      type: Object,
-      required: true
+  props: ['todo'],
+  data() {
+    return {
+      isEditing: false,
+    };
+  },
+  methods: {
+    showForm() {
+      this.isEditing = true;
+    },
+    hideForm() {
+      this.isEditing = false;
     }
-  }
-}
+  },
+};
 </script>
