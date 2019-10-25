bc19ccc0271a50b86ba3fbfb9e52da32ad569941
Jesper N. Pedersen <jeppenp12@gmail.com>
2019-10-25 14:13:01 +0200

    Frontend View Made + Routing System Made. Needs to clear data before getting new data

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import { firestorePlugin } from 'vuefire'
 import './registerServiceWorker'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faHeartbeat } from '@fortawesome/free-solid-svg-icons'
+import { faListUl } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(faHeartbeat)
+library.add(faBars)
+library.add(faListUl)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(firestorePlugin);
 Vue.config.productionTip = false
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,25 @@
 <template>
 	<div id="app">
 		<img src="img/do-long.svg" alt="Datodo logo" class="logo">  
-		<TodoList/>
+		<div id="panels">
+			<CategoryList/>
+			
+			<div id="task-panel">	
+			<h1>{{ $route.params.category }}</h1>
+			<TodoList/>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+
+import CategoryList from '../components/CategoryList.vue'
 import TodoList from '../components/TodoList.vue'
 
 export default {
 	components: {
-		TodoList
+		CategoryList, TodoList
 	}
 }
 </script>
